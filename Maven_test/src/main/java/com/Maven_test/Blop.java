752060ffd93de99097f7ccabd3686ca5818458e4
Alexandre <Alexandre@SERGE>
2018-01-09 08:05:20 +0100

    soustraction

--- Maven_test/src/main/java/com/Maven_test/Blop.java
+++
@@ -1,4 +0,0 @@
-package com.Maven_test;
-
-public class Blop {
-}
---
+++ Maven_test/src/main/java/com/Maven_test/Soustraction.java
@@ -0,0 +1,8 @@
+package com.Maven_test;
+
+public class Soustraction {
+	public Long calculer(Long a,  Long b){
+		return a-b;
+	}
+	
+}
